81b72f36bd7d646114bdcc6ebf20d09f8fc946c3
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-07 13:40:12 +0200

    inserisco la gestione input/output

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Prova 123");
+//costante costo per kilometro
+const kmPrice = 0.21;
+
+//costanti scontistiche
+const underAgeDisc = 0.8;
+const ove65Disc = 0.6;
+
+//seleziono gli input
+const kmInput = document.getElementById("kmForm");
+const ageInput = document.getElementById("ageForm");
+const myForm = document.querySelector("form");
+
+//seleziono l'outpout
+const myOut = document.getElementById("out");
+
+
+
+
+
